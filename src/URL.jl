17755cd34bd44de9981568cca73c3e1895b52361
miti <mitikoman494@gmail.com>
2024-12-13 15:16:43 +0330

    minor improvement

--- src/URL.jl
+++ src/URL.jl
@@ -17,7 +17,7 @@ struct URL
 	scheme::String                          # https
 	username::String                        # admin
 	password::String                        # test1234
-	auth::String                   # admin:test1234
+	auth::String                            # admin:test1234
 	host::String                            # login.admin-auth.company.co.com 
 	subdomain::String                       # login.admin-auth
 	domain::String                          # company
@@ -27,13 +27,13 @@ struct URL
 	directory::String                       # /admin/desk
 	file::String                            # master.js
 	file_name::String                       # master
-	file_ext::String                  # js
+	file_ext::String                        # js
 	query::String                           # A=line+25&B=#12&C
 	query_params::Vector{String}            # ["A", "B", "C"]
-	query_values::Vector{String}       # ["line+25", "#12"]
-	query_paires::OrderedDict{String, Any}# {"A":"line+25", "B":"#12", "C":null}
+	query_values::Vector{String}            # ["line+25", "#12"]
+	query_paires::OrderedDict{String, Any}  # {"A":"line+25", "B":"#12", "C":null}
 	query_params_count::Int32               # 3
-	query_values_count::Int32           # 2
+	query_values_count::Int32               # 2
 	fragment::String                        # justfortest
 
 	# From the beginning of URL to the given section
@@ -94,7 +94,7 @@ function split_domain(host::String)
 	file = isfile("tlds.txt") ? "tlds.txt" : "src/tlds.txt"
 	tlds = Set{AbstractString}()
 	for line in eachline(file)
-		occursin(Regex("\\b$line\\b\\Z"), host) && push!(tlds, line)
+		endswith(host, line) && push!(tlds, line)
 	end
 	tld = argmax(length, tlds)[2:end]
 
@@ -276,6 +276,6 @@ function Json(url::URL)
 			pop!(custom_json, key)
 		end
 	end
-	
+
 	push!(JSON_DATA, custom_json)
 end
